ea4c0c8d520abc1a32fcdf7ae2e9147c40f5a397
Nestor Daniel Sandoval Garvalena <nestordanielsg2001@gmail.com>
2024-11-24 19:22:37 -0600

    feat: add new Shadcn/UI certificate to the certificates section

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -5,8 +5,9 @@ const Certificates = () => {
         <h1 className="text-[30px] text-center font-black py-10">
           CERTIFICADOS
         </h1>
-        <div className="animate__animated animate__zoomInUp flex justify-center">
+        <div className="animate__animated animate__zoomInUp grid grid-cols-1 md:grid-cols-2 place-items-center space-y-10">
           <img src="Certificado .NET.png" width={600} />
+          <img src="Certificado ShadcnUI.png" width={500} />
         </div>
       </div>
     </section>
