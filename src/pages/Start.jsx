7fe514b52b20e0f80bce8bd20c41734c14fa52af
NestorSandoval <nestordanielsg2001@gmail.com>
2025-03-17 11:18:06 -0600

    chore: se cambio el estilo del colores de los iconos del dock

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -10,7 +10,7 @@ const Start = () => {
     {
       icon: (
         <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
+          class="w-6 h-6 text-white dark:text-white"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -38,7 +38,7 @@ const Start = () => {
     {
       icon: (
         <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
+          class="w-6 h-6 text-white dark:text-white"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -61,7 +61,7 @@ const Start = () => {
     {
       icon: (
         <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
+          class="w-6 h-6 text-white dark:text-white"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -89,7 +89,7 @@ const Start = () => {
     {
       icon: (
         <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
+          class="w-6 h-6 text-white dark:text-white"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           width="24"
